ef396a9cf67d9d273458d96a75148e629d7aea38
kamiokan <spherenov@gmail.com>
2019-09-22 14:18:03 +0900

    輸出・輸入、する・しない

---
+++ ch8/America.pm
@@ -0,0 +1,43 @@
+# America.pm -- 輸出国アメリカ
+package America;
+
+use strict;
+use warnings;
+use Exporter;
+our @ISA = qw(Exporter);
+
+our @EXPORT      = qw(beef orange car);
+our @EXPORT_OK   = qw(cd dvd);
+our @EXPORT_FAIL = "oil";
+our %EXPORT_TAGS = (
+    round  => [qw(orange cd dvd)],
+    hard   => [qw(car cd dvd)],
+    liquid => [qw(oil)],
+);
+
+sub beef {
+    print "I'm American BEEF! Buy me and eat me ! \n";
+}
+
+sub orange {
+    print "I'm American ORANGE! Buy me and eat me ! \n";
+}
+
+sub car {
+    print "I'm American CAR! Buy me and ride me ! \n";
+}
+
+sub cd {
+    print "I'm American CD! Well, you can buy me and listen to... \n";
+}
+
+sub dvd {
+    print "I'm American DVD! Well, you can buy me and watch... \n";
+}
+
+sub oil {
+    print "I'm American OIL! Well, you can never buy me ! \n";
+}
+
+1;
+
---
+++ ch8/Japan.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+#
+# Japan.pl -- 輸入国日本
+
+#use strict;
+#use warnings;
+use FindBin;
+use lib $FindBin::Bin;
+
+#use America;
+#use America "car";
+#use America "car", "cd";
+#use America ":round";
+#use America ":liquid";
+use America ();
+
+beef; orange; car; cd; dvd; oil;
+America::oil;
+
