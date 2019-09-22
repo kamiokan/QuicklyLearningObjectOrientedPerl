34cf32b13e0fc8fa8dc2a934da18fa1b56003407
kamiokan <spherenov@gmail.com>
2019-09-22 10:02:36 +0900

    7-1 パッケージとは何か

---
+++ ch7/packageExpLib.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+#
+# packageExpLib.pl
+# -- パッケージの実験（ライブラリ）
+
+package B;
+$n = 3;
+print "((a)) ", __PACKAGE__, " n:", $n, "\n";
+print "((b)) B::n:",    $B::n,    "\n";
+print "((c)) main::n:", $main::n, "\n";
+
+1;
+
