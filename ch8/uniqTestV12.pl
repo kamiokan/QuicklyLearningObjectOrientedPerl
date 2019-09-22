1e90f4adba6ac7f0b9920a13c9048272bcf1460a
kamiokan <spherenov@gmail.com>
2019-09-22 11:42:27 +0900

    8-1 ¤数について

---
+++ ch8/uniqTestV12.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+#
+# uniqTestV12.pl -- サブルーチン uniqのテスト（@VERSIONを修飾しない 動きません）
+
+use strict;
+use warnings;
+use FindBin;
+use lib $FindBin::Bin;
+use ArrUtil;
+
+print "ArrUtil $VERSION required!\n";
+
+my %uniq = ();
+my @in   = ( 5, 7, 3, 7, 3, 2, 5 );
+
+for my $in ( uniq(@in) ) {
+    if ( main_uniq($in) ) {
+        print "$in is unique!\n";
+    }
+    else {
+        print "$in is not unique!\n";
+    }
+}
+
+sub main_uniq {
+
+# 初めて渡される値の場合は真、２回目以降は偽を返すサブルーチン
+    my $in = shift;
+
+    if ( not $uniq{$in} ) {
+        $uniq{$in} = 1;
+        1;
+    }
+    else {
+        0;
+    }
+}
+
